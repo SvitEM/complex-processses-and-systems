85ce4d0351440710ae0e136775555ee8cd7eec68
Svit <Svyatosalv.EM@gmail.com>
2020-01-09 16:25:12 +0300

    say_hello in class

---
+++ Host.h
@@ -0,0 +1,24 @@
+//
+// Created by Svyatoslav Emelyanenko on 08.01.2020.
+//
+
+#ifndef LAB1__HOST_H
+#define LAB1__HOST_H
+
+
+class Host {
+public:
+    Host();
+
+    ~Host();
+
+    void say();
+
+private:
+    void *dylib;
+
+    void (*say_hello)();
+};
+
+
+#endif //LAB1__HOST_H
--- main.cpp
+++ main.cpp
@@ -8,31 +8,36 @@
 
 #include "library.h"
 
+#include "Host.h"
+
 using namespace std;
 
+
 int main(int argc, const char *argv[]) {
-    const char *filename = "./liblab1_shared.dylib";
-
-    // open libtest.dylib
-    void *dylib = dlopen(filename, RTLD_LAZY);
-
-    if (dylib == nullptr) {
-        cout << "unable to load " << filename << " Library!" << endl;
-        return 1;
-    }
-
-    // get print function from libtest.dylib
-    auto say_hello = (void (*)()) dlsym(dylib, "say_hello");
-
-    if (say_hello == nullptr) {
-        cout << "unable to load " << filename << " function!" << endl;
-        dlclose(dylib);
-        return 2;
-    }
+//    const char *filename = "./liblab1_shared.dylib";
+//
+//    void *dylib = dlopen(filename, RTLD_LAZY);
+//
+//    if (dylib == nullptr) {
+//        cout << "unable to load " << filename << " Library!" << endl;
+//        return 1;
+//    }
+//
+//    // get print function from libtest.dylib
+//    auto say_hello = (void (*)()) dlsym(dylib, "say_hello");
+//
+//    if (say_hello == nullptr) {
+//        cout << "unable to load " << filename << " function!" << endl;
+//        dlclose(dylib);
+//        return 2;
+//    }
 
     // test the print function
-    say_hello();
+//    say_hello();
 
-    dlclose(dylib);
+//    dlclose(dylib);
+
+    Host A;
+    A.say();
     return 0;
 }
